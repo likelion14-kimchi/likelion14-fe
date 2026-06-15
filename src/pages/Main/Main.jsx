20ad4a93652b5b13959af99769aded4281de6f15
seungmin <ssmmkim1025@naver.com>
2026-06-15 20:00:40 +0900

    :sparkles: Feat: 버튼 컴포트 구현

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,7 +1,27 @@
+import Button from "../../components/button/Button";
+import styled from "styled-components";
+
 export default function Main() {
   return (
-    <div>
-      <div></div>
-    </div>
+    <Wrapper>
+      <ButtonBox>
+        <Button buttonName="구독하기" $active={false} />
+      </ButtonBox>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  position: relative;
+`;
+
+const ButtonBox = styled.div`
+  position: absolute;
+  bottom: 135px;
+  
+  display: flex;
+  justify-content: center;
+  width: 100%;
+`;
